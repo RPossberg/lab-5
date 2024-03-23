a24ce57000691a9155ee69ed5c336abc5213229d
Wizzard2001 <rpossberg1@nait.ca>
2024-03-23 16:56:36 -0600

    Refactor event listeners and add filterGames and addToFavouriteGames functions in main.js

--- js/main.js
+++ js/main.js
@@ -9,13 +9,14 @@ Steps 1-3 READ THE PDF!
   let myGameList = document.querySelector(".my-favourite-games");
   let myGames = [];
 
-  // event listener for step 1
+  // event listener for step 1 - filter games
   videoGameForm.addEventListener("submit", (event) => {
     event.preventDefault();
     let platform = event.target.elements["platform-family"].value.toLowerCase();
     filterGames(platform);
   });
 
+  // Create a function called filterGames that takes in a platform as an argument. This function should filter the games based on the platform and hide the games that do not match the platform.
   function filterGames(platform) {
     allGameListItems.forEach((item) => {
       if (item.innerText.toLowerCase().includes(platform)) {
@@ -26,12 +27,14 @@ Steps 1-3 READ THE PDF!
     });
   }
 
-  // event listener for step 2
+  // event listener for step 2 - add to favourites list
   allGameList.addEventListener("click", (event) => {
     let game = event.target.innerText;
     addToFavouriteGames(game);
+    console.log(myGames);
   });
 
+  // Create a function called addToFavouriteGames that takes in a game as an argument. This function should add the game to the myGames array and then call the addToFavouriteGames function. Within the event handler, call the function addToFavouriteGames and pass in the game that was clicked on.
   function addToFavouriteGames(game) {
     myGames.push(game);
     renderFavouriteList();
